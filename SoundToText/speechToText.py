dd99ab27b9c558f316c9509a3c173b9f14f20a85
Emilo Gopaul <emilo@virtanatech.com>
2021-09-29 19:53:18 -0400

    A speech to text converter

---
+++ SoundToText/speechToText.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import speech_recognition as sr
+
+
+# initialize the recognizer
+r = sr.Recognizer()
+
+time = float(input("How long you wanna listen for boo (in minutes)? "))
+time = time * 60
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=int(time))
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
